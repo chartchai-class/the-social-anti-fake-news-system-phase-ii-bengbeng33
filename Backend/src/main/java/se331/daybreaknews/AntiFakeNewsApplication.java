61c52c56b3aa683ed356435de066553a81196a6b
Evan Clark <black0ashes@gmail.com>
2025-10-28 12:29:39 +0700

    Project Setup & Folder Structure

---
+++ Backend/src/main/java/se331/daybreaknews/AntiFakeNewsApplication.java
@@ -0,0 +1,12 @@
+package se331.daybreaknews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiFakeNewsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AntiFakeNewsApplication.class, args);
+    }
+}
+
